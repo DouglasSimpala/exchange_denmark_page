ab2b2b0637cd380a2c7af412f76ad30c92e96753
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-10-08 16:52:58 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
 
     // Exemplo de valor arrecadado e meta
     const totalCampaignGoal = 68378.20;  // Meta de arrecadação total
-    const amountRaised = 11192.29;              // Quantia arrecadada até agora
+    const amountRaised = 11242.29;              // Quantia arrecadada até agora
     const remainingAmount = parseFloat((totalCampaignGoal - amountRaised).toFixed(2));  // Quantia faltante
 
     donationChart = new Chart(ctx, {
